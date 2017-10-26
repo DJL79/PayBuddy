64d6d392dfb0483f1f5aa3c4ffba3a6d8ae22798
Dylan Lawless (CA Account) <ca_K1517669@kclad.ds.kcl.ac.uk>
2017-10-26 09:24:22 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/boonapp/paybuddy/PayPalConfig.java
@@ -0,0 +1,10 @@
+package uk.co.boonapp.paybuddy;
+
+/**
+ * Created by ca_K1517669 on 18/10/2017.
+ */
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AdnFXlhe0PHN9pkwfJAd2Eq41lLigW4BWXBcjUZNdYAGQt3ryoO8mUcD-0rudUAjkiL4QuF78G9NtkxG";
+}
